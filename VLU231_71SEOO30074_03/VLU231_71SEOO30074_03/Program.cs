4dab54cf5b1f2f2fff6672a5eaac2ecc9953eeed
TranThangBin <thangdev04@gmail.com>
2023-12-05 16:10:33 +0700

    feat: project setup

---
+++ VLU231_71SEOO30074_03/VLU231_71SEOO30074_03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using VLU231_71SEOO30074_03.src.GUI;
+
+namespace VLU231_71SEOO30074_03
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Index());
+        }
+    }
+}
---
+++ VLU231_71SEOO30074_03/VLU231_71SEOO30074_03/src/GUI/Index.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace VLU231_71SEOO30074_03.src.GUI
+{
+    public partial class Index : Form
+    {
+        public Index()
+        {
+            InitializeComponent();
+        }
+    }
+}

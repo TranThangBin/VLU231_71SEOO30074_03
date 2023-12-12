f18cb5258b9c54ffbb41208eb5fc138cdcda8a3e
HoangPhuc2709 <phuctran27092004@gmail.com>
2023-12-13 00:05:49 +0700

    Khoa, MonHoc

--- VLU231_71SEOO30074_03/VLU231_71SEOO30074_03/src/DBModel.Designer.cs
+++ VLU231_71SEOO30074_03/VLU231_71SEOO30074_03/src/DBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\VL\OOP\Project\VLU231_71SEOO30074_03\VLU231_71SEOO30074_03\src\DBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\HK1 2023\OOP\VLU231_71SEOO30074_03\VLU231_71SEOO30074_03\VLU231_71SEOO30074_03\src\DBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

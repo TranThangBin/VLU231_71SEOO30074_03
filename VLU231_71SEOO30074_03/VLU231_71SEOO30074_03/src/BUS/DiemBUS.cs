11c66b3a2e05eeb17a33b9c9d1cbe8d84c227fcc
Phan Anh Khoa <129307664+pakhoakuy66@users.noreply.github.com>
2023-12-13 20:35:33 +0700

    LopHp, Diem (#2)

---
+++ VLU231_71SEOO30074_03/VLU231_71SEOO30074_03/src/BUS/DiemBUS.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VLU231_71SEOO30074_03.src.BUS
+{
+    internal class DiemBUS
+    {
+        public static void InsertDiem(string maSinhVien, string maHp)
+        {
+            Diem grade = new Diem() { MaSinhVien = maSinhVien, MaHp = maHp, };
+            using (var db = new QLDKHPEntities())
+            {
+                db.Diems.Add(grade);
+                db.SaveChanges();
+            }
+        }
+
+        public static void UpdateDiem(Diem DiemMoi, string maSinhVien, string maHp)
+        {
+            using (var db = new QLDKHPEntities())
+            {
+                Diem grade = db.Diems.SingleOrDefault(d => d.MaSinhVien == maSinhVien && d.MaHp == maHp);
+                if (grade == null)
+                {
+                    return;
+                }
+                grade.DiemTrongLop = DiemMoi.DiemTrongLop;
+                grade.DiemGiuaKy = DiemMoi.DiemGiuaKy;
+                grade.DiemCuoiKy = DiemMoi.DiemCuoiKy;
+                db.SaveChanges();
+            }
+        }
+
+        public static void deleteDiem(string maSinhVien, string maHp)
+        {
+            using (var db = new QLDKHPEntities())
+            {
+                Diem grade = db.Diems.SingleOrDefault(d => d.MaSinhVien == maSinhVien && d.MaHp == maHp);
+                if (grade == null)
+                {
+                    return;
+                }
+                db.Diems.Remove(grade);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ VLU231_71SEOO30074_03/VLU231_71SEOO30074_03/src/BUS/LopHpBUS.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VLU231_71SEOO30074_03.src.BUS
+{
+    internal class LopHpBUS
+    {
+        public static void InsertLopHp(string maMonHoc)
+        {
+            LopHp lophp = new LopHp() { MaMonHoc = maMonHoc };
+            using (var db = new QLDKHPEntities())
+            {
+                db.LopHps.Add(lophp);
+                db.SaveChanges();
+            }
+        }
+
+        public static void updateLopHp(string maLopHP, LopHp lopHpMoi)
+        {
+            using (var db = new QLDKHPEntities())
+            {
+                LopHp lophp = db.LopHps.Find(maLopHP);
+                if (lophp == null)
+                {
+                    return;
+                }
+                lophp.Ma = lopHpMoi.Ma;
+                db.SaveChanges();
+            }
+        }
+
+        public static void deleteLopHp(string maLopHP)
+        {
+            using (var db = new QLDKHPEntities())
+            {
+                LopHp hp = db.LopHps.Find(maLopHP);
+                if (hp == null)
+                {
+                    return;
+                }
+                db.LopHps.Remove(hp);
+                db.SaveChanges();
+            }
+        }
+    }
+}
